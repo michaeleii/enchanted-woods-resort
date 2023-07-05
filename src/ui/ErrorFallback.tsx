7f262fced0b83d71996602632a4bcd0857c6e41b
Michael Lei <michaelleie0ca@gmail.com>
2023-07-04 21:55:32 -0700

    Add error boundary

--- src/ui/ErrorFallback.tsx
+++ src/ui/ErrorFallback.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,26 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: {
+  error: Error;
+  resetErrorBoundary: () => void;
+}) {
+  return (
+    <GlobalStyles>
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🤔</Heading>
+          <p>{error.message}</p>
+          <Button onClick={resetErrorBoundary} size="large">
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </GlobalStyles>
+  );
+}
+export default ErrorFallback;
